d1d36220070e3222032d2a95538f56f8870f8dbe
Dung-Tran-1995 <60059937+Dung-Tran-1995@users.noreply.github.com>
2022-07-06 17:39:29 +0700

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-function App() {
+function App () {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+        <header className='header'>
+          <div className='Dicon'>
+            <i class="fa-solid fa-d"></i>
+          </div>
+          <nav>
+            <ul>
+              <li><a>Me</a></li>
+              <li><a>Service</a></li>
+              <li><a>Contact</a></li>
+              <li><a>About Me</a></li>
+              <button> Contact Me</button>
+            </ul>
+          </nav>
+        </header>
+      </section>
     </div>
   );
 }
